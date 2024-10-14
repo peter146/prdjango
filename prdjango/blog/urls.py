9830f507d2896415dea634f86f217dc2ca81f50f
peter <peter146@gmail.com>
2024-10-14 14:45:48 +0200

    repair html display

--- prdjango/blog/urls.py
+++ prdjango/blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('', include('blog.urls')),
 ]
