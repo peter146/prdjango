e876d4ae42de957a114ea73d056724b622773688
peter <peter146@gmail.com>
2024-10-21 10:55:55 +0200

    html template creation

--- prdjango/blog/views.py
+++ prdjango/blog/views.py
@@ -4,4 +4,4 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts}) #if the dictionary with the posts variable is missing, no posts will be displayed!
--- prdjango/prdjango/settings.py
+++ prdjango/prdjango/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # for production (deployment), not needed for development
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'prdjango', 'blog', 'static'), # adjust path if necessary
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
