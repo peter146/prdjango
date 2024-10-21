d5b9169b722ffbe028dd5ef80c64b83a48dc4930
peter <peter146@gmail.com>
2024-10-21 14:01:01 +0200

    working on les 6, not done yet

--- prdjango/blog/views.py
+++ prdjango/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts}) #if the dictionary with the posts variable is missing, no posts will be displayed!
+
+def post_detail(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+    except Post.DoesNotExist: 
+        print(f'Post with PK {pk} does not exist.')
+    return render(request, 'blog/post_inhoud.html', {'post': post})
