a56e0d8382d4488c985f50f3919c19f4399ab552
peter <peter146@gmail.com>
2024-10-28 10:36:43 +0100

    form and delete option

--- prdjango/blog/urls.py
+++ prdjango/blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+    path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+    path('delete/<post_id>', views.post_delete, name='delete')
 ]
--- prdjango/blog/views.py
+++ prdjango/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,8 +13,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_inhoud.html', {'post': post})
 
-    # try:
-    #     post = get_object_or_404(Post, pk=pk)
-    # except Post.DoesNotExist: 
-    #     print(f'Post with PK {pk} does not exist.')
-    # return render(request, 'blog/post_inhoud.html', {'post': post})
+def post_nieuw(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_inhoud', pk=post.pk)
+    else:
+        form = PostForm
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, post_id=None):
+    post_to_delete=Post.objects.get(id=post_id)
+    post_to_delete.delete()
+    return render(request, 'blog/post_list.html')
